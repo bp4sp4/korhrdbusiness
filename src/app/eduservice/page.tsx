0eac5725d08e34acade310ebb4ecf532cd41621a
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-07-02 17:25:26 +0900

    mobile update

--- src/app/eduservice/page.tsx
+++ src/app/eduservice/page.tsx
@@ -131,8 +131,8 @@ export default function Eduservice() {
           <h2 className="text-[32px] md:text-[40px] text-[#1E1E1E] font-extrabold text-left md:text-right leading-tight mb-4">
             내일의 전문가, 오늘 지원하세요
           </h2>
-          <p className="text-[16px] md:text-[20px] text-[#979797]  text-left md:text-left">
-            목표를 현실로, 한평생이 도와드립니다.
+          <p className="text-[16px] md:text-[20px] text-[#1E1E1E]  text-left md:text-left">
+            목표를 현실로, <strong>한평생이 도와드립니다.</strong>
           </p>
         </motion.div>
       </section>
