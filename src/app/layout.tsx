0d3fc62c5170d97d38c1c6415aa88a99b0ea7ac8
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-07-16 10:24:01 +0900

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
   metadataBase: new URL("https://www.eduvisor.kr/"),
   alternates: {
